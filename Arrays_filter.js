e40554f2fab6a9f14884dbef36965ddfd2013978
VSTech-solutions-trainings <vstechsol.trainings@gmail.com>
2025-07-08 18:07:35 -0500

    July 08, Arrow functions, Map, reduce, filter

---
+++ Arrays_filter.js
@@ -0,0 +1,15 @@
+//filter
+
+//filter out the elements based on a condition
+
+let arr = [1,2,3,4]
+
+//requirement - filter even numbers - [2,4]
+
+let filteredArr =  arr.filter((ele) => {
+    if([]) {
+        return 'random texts';
+    }
+})
+
+console.log(filteredArr)
---
+++ function_statements-expressions.js
@@ -0,0 +1,15 @@
+//statements, expressions
+
+//function statement
+
+function printName() {
+    console.log('Mike')
+}
+
+//printName()
+
+//function expression
+var printName = function() {
+    console.log('Mike')
+}
+printName()
